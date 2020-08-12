44e77245b06064fe67168acfe691e1e83dd3ff77
wangzhen <1486499203@qq.com>
2020-08-12 11:29:29 +0800

    安卓发布配置文件

--- lib/net/url_constant.dart
+++ lib/net/url_constant.dart
@@ -1,7 +1,7 @@
 class UrlConstant {
 
-  static const String host = 'http://172.18.0.94:9009/';//测试
-//  static const String host = 'http://oa.wanmagroup.com/';//正式
+//  static const String host = 'http://172.18.0.94:9009/';//测试
+  static const String host = 'http://oa.wanmagroup.com/';//正式
 
   ///获取会议数据
   static getMeetingDatas() {
